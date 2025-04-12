7c3310cc7a77c80b80fbc1388ea141d9a40931ae
yohanlb <yohan.lbtn@proton.me>
2025-04-12 19:29:40 +0200

    fix: change HTML language attribute from English to French

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={ibmPlexSans.className}>{children}</body>
     </html>
   );
