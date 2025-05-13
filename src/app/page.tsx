0f21f850bda941f981cb692f9bfd9facc684a2f8
yohanlb <yohan.lbtn@proton.me>
2025-05-13 22:19:24 +0200

    feat: remove coming soon line and add opening hours, and add google map link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,14 +19,22 @@ export default function Home() {
         <div className="flex flex-col items-center text-md leading-[17px] md:leading-[30px] font-[400] md:text-xl lg:text-2xl">
           <div>CAFÉ / THÉ / ATELIERS</div>
           <div>®2025 PARIS</div>
-          <div>OPENING SOON</div>
+          <div className="flex flex-col items-center mt-2 text-sm md:text-base">
+            <span>MERCREDI À VENDREDI 8:30 – 17:30</span>
+            <span>SAMEDI / DIMANCHE 10:00 – 18:00</span>
+          </div>
         </div>
       </div>
       {/* Address section */}
-      <div className="text-md md:text-xl lg:text-2xl text-center leading-5 md:leading-8 font-[400] md:mb-2">
+      <a
+        href="https://maps.app.goo.gl/xykKssnuFmrHMATy7"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-md md:text-xl lg:text-2xl text-center leading-5 md:leading-8 font-[400] md:mb-2 hover:underline"
+      >
         <p>10 PASSAGE THIÉRÉ,</p>
         <p>75011, PARIS</p>
-      </div>
+      </a>
       <Footer />
     </main>
   );
