245634070bf6b725dc27b3c178682d24abdee113
yohanlb <yohan.lbtn@proton.me>
2025-05-18 17:03:01 +0200

    style: reorganize layout for logo, subtitle, and opening hours; enhance address section with semantic markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,40 +7,52 @@ export default function Home() {
       {/* Future Navbar */}
       <nav className="h-18 w-full"></nav>
       {/* Logo and subtitle group */}
-      <div className="flex-grow flex flex-col items-center justify-center gap-5 md:gap-8 mx-8">
+      <div className="flex-grow flex flex-col items-center justify-center mx-8">
+        {/* Logo */}
         <Image
           src="/images/poesie-logo.svg"
           alt="Poésie"
           width={300}
           height={120}
           priority
-          className="w-full h-auto max-w-[500px] lg:max-w-[600px]"
+          className="w-full h-auto max-w-[500px] lg:max-w-[600px] my-2"
         />
-        <div className="flex flex-col items-center text-md leading-[17px] w-full max-w-[400px] lg:max-w-[500px] md:leading-[30px] font-[400] md:text-xl lg:text-2xl gap-25">
-          <div className="flex flex-col items-center gap-0.5">
-            <div>CAFÉ / THÉ / ATELIERS</div>
-            <div>®2025 PARIS</div>
-          </div>
-          <div className="flex flex-col items-center w-full font-normal gap-0.5">
-            <p className="flex justify-between w-full ">
-              <span>MERCREDI - VENDREDI</span> <span>8:30 – 17:30</span>
-            </p>
-            <p className="flex justify-between w-full ">
-              <span>SAMEDI - DIMANCHE</span> <span>10:00 – 18:00</span>
-            </p>
-          </div>
+
+        {/* Subtitle */}
+        <div className="flex flex-col items-center leading-tight text-md md:text-xl lg:text-2xl">
+          <div>CAFÉ / THÉ / ATELIERS</div>
+          <div>®2025 PARIS</div>
+        </div>
+
+        {/* Separator */}
+        <div className="h-28 md:h-36"></div>
+
+        {/* Opening hours */}
+        <div className="flex flex-col items-center font-normal leading-tight text-md w-full max-w-[400px] lg:max-w-[500px] md:leading-[30px] md:text-xl lg:text-2xl">
+          <p className="flex justify-between w-full">
+            <span>MERCREDI - VENDREDI</span>
+            <span>8:30 – 17:30</span>
+          </p>
+          <p className="flex justify-between w-full">
+            <span>SAMEDI - DIMANCHE</span>
+            <span>10:00 – 18:00</span>
+          </p>
         </div>
       </div>
-      {/* Address section */}
-      <a
-        href="https://maps.app.goo.gl/xykKssnuFmrHMATy7"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-md md:text-xl lg:text-2xl text-center leading-5 md:leading-8 font-[400] md:mb-2 hover:underline"
-      >
-        <p>10 PASSAGE THIÉRÉ,</p>
-        <p>75011, PARIS</p>
-      </a>
+
+      {/* Address */}
+      <address className="not-italic">
+        <a
+          href="https://maps.app.goo.gl/xykKssnuFmrHMATy7"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-md md:text-xl lg:text-2xl text-center leading-5 md:leading-8 font-normal md:mb-2 hover:underline"
+        >
+          <p>10 PASSAGE THIÉRÉ,</p>
+          <p>75011, PARIS</p>
+        </a>
+      </address>
+
       <Footer />
     </main>
   );
