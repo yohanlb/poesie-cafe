1b785a28112b7af6d565b121392ebfdf1fc13db0
yohanlb <yohan.lbtn@proton.me>
2025-05-18 15:16:54 +0200

    style: enhance layout and spacing for logo and opening hours section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,27 @@ export default function Home() {
       {/* Future Navbar */}
       <nav className="h-18 w-full"></nav>
       {/* Logo and subtitle group */}
-      <div className="flex-grow flex flex-col items-center justify-center gap-5 md:gap-8">
+      <div className="flex-grow flex flex-col items-center justify-center gap-5 md:gap-8 mx-8">
         <Image
           src="/images/poesie-logo.svg"
           alt="Poésie"
           width={300}
           height={120}
           priority
-          className="w-auto h-auto max-w-full md:w-[500px] lg:w-[600px]"
+          className="w-full h-auto max-w-[500px] lg:max-w-[600px]"
         />
-        <div className="flex flex-col items-center text-md leading-[17px] md:leading-[30px] font-[400] md:text-xl lg:text-2xl">
-          <div>CAFÉ / THÉ / ATELIERS</div>
-          <div>®2025 PARIS</div>
-          <div className="flex flex-col items-center mt-2 text-sm md:text-base">
-            <span>MERCREDI À VENDREDI 8:30 – 17:30</span>
-            <span>SAMEDI / DIMANCHE 10:00 – 18:00</span>
+        <div className="flex flex-col items-center text-md leading-[17px] w-full max-w-[400px] lg:max-w-[500px] md:leading-[30px] font-[400] md:text-xl lg:text-2xl gap-25">
+          <div className="flex flex-col items-center gap-0.5">
+            <div>CAFÉ / THÉ / ATELIERS</div>
+            <div>®2025 PARIS</div>
+          </div>
+          <div className="flex flex-col items-center w-full font-normal gap-0.5">
+            <p className="flex justify-between w-full ">
+              <span>MERCREDI - VENDREDI</span> <span>8:30 – 17:30</span>
+            </p>
+            <p className="flex justify-between w-full ">
+              <span>SAMEDI - DIMANCHE</span> <span>10:00 – 18:00</span>
+            </p>
           </div>
         </div>
       </div>
