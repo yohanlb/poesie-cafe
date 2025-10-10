dc233801477ceb03aa5a8e30131ed2701a3c6f47
yohanlb <yohan.lbtn@proton.me>
2025-10-10 19:59:09 +0200

    style: adjust footer text size and color for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="h-11 flex justify-between items-center px-4">
-      <p className="text-xs text-white/60 text-center flex-1">
+      <p className="text-[10px] md:text-xs text-white/40 md:text-white/60 text-center flex-1">
         Design by Axelle Brisard / Developed by{" "}
         <a
           href="https://yohanlebreton.com"
           target="_blank"
           rel="dofollow"
-          className="hover:text-white/80 transition-colors"
+          className="hover:text-white/60 md:hover:text-white/80 transition-colors"
         >
           Yohan Le Breton
         </a>
